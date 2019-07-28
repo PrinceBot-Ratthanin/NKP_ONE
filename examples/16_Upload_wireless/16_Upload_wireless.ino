0b5daea1bb365844b14d716a7f7c734275d332ee
PrinceBot-Ratthanin <50706633+PrinceBot-Ratthanin@users.noreply.github.com>
2019-07-28 13:13:52 +0700

    Update 16_Upload_wireless.ino

--- examples/16_Upload_wireless/16_Upload_wireless.ino
+++ examples/16_Upload_wireless/16_Upload_wireless.ino
@@ -2,8 +2,8 @@
 
 void setup() {
   NKP_ONE();
-  OTA_Open("PrinceBot","aaaaaaaa");
-  OTA_SW1();   
+  OTA_Open("PrinceBot","aaaaaaaa");   //ตั้ง Hostpot เป็นชื่อ PrinceBot  แล้วตั้ง รหัสผ่านเป็น aaaaaaaa  
+  OTA_SW1();                          //รอจนกว่าจะกด SW1 เพื่อทำงาน 
 
 
 }
