bce196349a769a86511073a2de747d440207b905
PrinceBot-Ratthanin <50706633+PrinceBot-Ratthanin@users.noreply.github.com>
2019-07-28 11:19:43 +0700

    Update 14_TC_Sensor.ino

--- examples/14_TC_Sensor/14_TC_Sensor.ino
+++ examples/14_TC_Sensor/14_TC_Sensor.ino
@@ -2,7 +2,7 @@
 
 void setup() {
   Serial.begin(115200);
-  setSensorPins((const uint8_t[]){A0, A1, A2 , A3}, 4);
+  setSensorPins((const int[]){A0, A1, A2 , A3}, 4);
   // setSensorMax((const uint16_t[]){2400, 4001,4002,3500});
   // setSensorMin((const uint16_t[]){300, 301, 302, 303});
   for(int i = 0;i<5000;i++){
